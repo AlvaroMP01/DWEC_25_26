282115a8bc50869eeaf383b2926299a6d89f1fb4
AlvaroMP <alvaromorcillo2001@gmail.com>
2025-09-29 16:57:00 +0200

    modiffied ejercicio5

--- semana01/src/ejercicio5.js
+++ semana01/src/ejercicio5.js
@@ -17,11 +17,28 @@ const unirArraysAF= (array1=[], array2=[]) => [...array1, ...array2];
 
 //? Arrays de numeros y quiero calcular el cuadrado de los números
 const numeros = [1,2,3,4,5];
-/*
+
+/**
+* @param {number[]} - array de números
+* @returns {number[]} - array de números al cuadrado
+* @example
+* cuadrado([1,2,3,4,5]) // [1,4,9,16,25]
+*/
+
+
 function cuadrado(array = []){
     return array.map((element) => element * element);
 }
-*/
+
 const cuadradoAF = (array=[]) => array.map((element) => element * element);
 
-console.log(`El cuadrado del array; ${numeros} = ${cuadradoAF(numeros)}`);
+console.log(`El cuadrado del array usando Arrow Function es: ${numeros} = ${cuadradoAF(numeros)}`);
+console.log(`El cuadrado del array es: ${numeros} = ${cuadrado(numeros)}`);
+
+//? 1º Crear tres funciones que les pases un array de numeros y me diga tres cosas: el maximo, el minimo y un array con los numeros que son primos
+const numeros = [10,20,15,12,21,68]
+
+function maximo(array = []) {
+    return Math.max(...array);
+}
+
